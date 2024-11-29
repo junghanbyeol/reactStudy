9f18f2a4a844090072f2945991c198d5ca7b1c3f
junghanbyeol <gksquf9720@naver.com>
2024-11-29 10:40:05 +0900

    컴포넌트 생성,파괴 시 호출 되는 함수 연습

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@
 import Button from "./Button";
 import sytles from "./App.module.css"
 
+import { useState, useEffect } from "react";
 
-function App() {
+function Hello() {
+  
+  useEffect(() => {
+    console.log('hi')
+    return () => console.log('bye')
+  }, [])
   return (
     <div>
-      <h1 className={sytles.title}>Hello World!!!</h1>
-      <Button text={"CHIHI"} />
+      <h1>Hello</h1>
+    </div>
+  );
+}
+function App() {
+  const [showing, setShowing] = useState(false)
+  const onClick = () => setShowing((prev) => !prev)
+  return (
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : 'Show'}</button>
     </div>
   );
 }
