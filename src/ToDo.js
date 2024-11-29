7fdfbe5f8ed0765d8e3783f7d73ffb961da5780f
junghanbyeol <gksquf9720@naver.com>
2024-11-29 11:16:12 +0900

    todo list 컴포넌트화

---
+++ src/ToDo.js
@@ -0,0 +1,37 @@
+
+import { useState, useEffect } from "react";
+
+function ToDo() {
+  const [toDo, setToDo] = useState("")
+  const [toDos, setToDos] = useState([])
+  const onChange = (event) => setToDo(event.target.value)
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === "") return
+    setToDos(currentArray => [toDo, ...currentArray])
+    setToDo("")
+  }
+  console.log(toDos)
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="할일을 입력해주세요 ..." 
+        />
+        <button>할 일 추가</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => <li key={index}>{item}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+export default ToDo;
+
+
