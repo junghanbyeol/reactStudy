c0ab4e5a55e707da0430491138a6db7c501e849f
junghanbyeol <gksquf9720@naver.com>
2024-11-29 11:12:14 +0900

    todo list

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
 
-import Button from "./Button";
-import sytles from "./App.module.css"
-
 import { useState, useEffect } from "react";
 
-function Hello() {
-  
-  useEffect(() => {
-    console.log('hi')
-    return () => console.log('bye')
-  }, [])
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
-}
 function App() {
-  const [showing, setShowing] = useState(false)
-  const onClick = () => setShowing((prev) => !prev)
+  const [toDo, setToDo] = useState("")
+  const [toDos, setToDos] = useState([])
+  const onChange = (event) => setToDo(event.target.value)
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === "") return
+    setToDos(currentArray => [toDo, ...currentArray])
+    setToDo("")
+  }
+  console.log(toDos)
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : 'Show'}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="할일을 입력해주세요 ..." 
+        />
+        <button>할 일 추가</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => <li key={index}>{item}</li>)}
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
